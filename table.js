48624e6aa0ec5654fc275da0424ea996d0c8a341
pranavikadatla <datlapranavika205@gmail.com>
2023-09-23 21:49:34 +0530

    array added

--- table.js
+++ table.js
@@ -1,18 +1,21 @@
-const tbody=document.getElementById("tbody");
-const tr=document.createElement("tr");
+
 fetch('./table.json')
 .then(res=>res.json())
 .then(data=>{
+    let tbody=document.getElementById("tbody");
+    let x="";
     data.forEach(element => { 
-        tr.innerHTML=`<td>${element.id}</td>
+                x+=`<tr>
+                      <td>${element.id}</td>
                       <td>${element.first_name}</td>
                       <td>${element.gender}</td>
                       <td>${element.class}</td>
                       <td>${element.marks}</td>
                       <td>${element.passing}</td>
-                      <td>${element.email}</td>`; 
-                      tbody.appendChild(tr);
-                      console.log(tbody);
+                      <td>${element.email}</td>
+                                          </tr>`; 
+                     
     })
+    tbody.innerHTML=x;
      
 });
